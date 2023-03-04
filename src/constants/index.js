e77eb396c5be062d331ec66c0eb402afeedd3294
seiftd <seiftouatilol@gmail.com>
2023-03-04 20:18:58 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,267 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    
+  } from "../assets";
+
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React JS Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Booking Hotels",
+      icon: reactjs,
+      iconBg: "#383E56",
+      date: "Jan 2022 - June 2022",
+      points: [
+        "In the graduation project  to obtain a certificate:State engineer in computer programming.",
+        "It is my lot to get a graduation topic:Booking hotel app, While searching for the best programming languages ​​for the web application .",
+        "I chose to be the front-end language: React js and back-end language:MongoDB.",
+        "During a research work on a project, I became a professional in the React js ,also Other related libraries.",
+      ],
+    },
+    {
+      title: "Multi- Developer",
+      company_name: "Multi-languge",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "June 2022 - September 2022",
+      points: [
+        "During this period, I was hesitant about which language to start with, which programming language would help me to become.",
+        "Rich or professional programmer .",
+        "I took an idea on the language of the dart,java.",
+        "I also wanted to develop games for Android,Or applications where I started learning on the flutter platform.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "figma",
+      icon: figma,
+      iconBg: "#383E56",
+      date: "September 2022 - Jan 2023",
+      points: [
+        "The period was also turbulent, but I decided to learn the language or platforme :figma.",
+        "Although it is a short period.",
+        "But I got a lot out of it,It was the reason I chose to work as free programmer.",
+        "Of course, I learned other languages ​​besides studying Software engineering.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "BIMS",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "It was the real beginning.",
+        "Our creation of the BMS team made it possible to choose one programming language .",
+        "Specialize in it and learn it to reach professionalism,And that's why I chose.",
+        "Developing and maintaining web applications using React.js and other related technologies.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Programmer specialized in web programming application :Html/css/js,She also has knowledge of other languages.",
+      name: "Allam Sofia",
+      designation: "Member",
+      company: "BIMS",
+      image: "https://scontent.faae2-2.fna.fbcdn.net/v/t39.30808-6/330974338_1599816223782111_8724698227891646341_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFGuCS4XLzzQ8L1Wuk6HAjr4aPCalAxty7ho8JqUDG3LsmQzQHLrAfscAZezZx08SsOuLVVj9lkVWjWjSWhk7Ar&_nc_ohc=GrLl_J3dfCYAX_oAUNM&_nc_ht=scontent.faae2-2.fna&oh=00_AfCiPRlaK0J_fvTuAisLVtZx39nt6JPp_a1MpolZj38Npw&oe=6407A6C5",
+    },
+    {
+      testimonial:
+        "Programmer specialized in web programming application specialized in the PHP language,She also has knowledge of other languages.",
+      name: "talbi ilhem",
+      designation: "Member",
+      company: "BIMS",
+      image: "https://scontent.faae2-2.fna.fbcdn.net/v/t39.30808-6/272309589_133145209191922_58740986066908986_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGTin5GyOazpAZx6QYFNZ4Z_Kld_tkYIaL8qV3-2RgholtjetexjMb5mNNxtDCxsQ2WOle_14-Z31UV_BEU8nD3&_nc_ohc=XATri_ovBt0AX9Fo3Co&_nc_ht=scontent.faae2-2.fna&oh=00_AfAAbPYM3B3n0Nx6kLmxAsbfB6E-0-nwby1jeZPR8xVtxg&oe=6408EED5",
+    },
+    {
+      testimonial:
+        "Programmer specialized in c# language,Has knowledge of the Java language,Obtained a degree in software engineering",
+      name: "Billal Ghomrani",
+      designation: "Member",
+      company: "BIMS",
+      image: "https://scontent.faae2-2.fna.fbcdn.net/v/t39.30808-6/320370606_3205627889750322_4937190100836572040_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEvl2f5m-Cu7U_NJ8yfid5Hn6u5lfbSdhSfq7mV9tJ2FBHcSdJ02nEUR6CbzB6BOd9cviXcA--LeNvDUgkWAlij&_nc_ohc=nxzkK6f10NgAX8J76xV&_nc_ht=scontent.faae2-2.fna&oh=00_AfDjDkj-gE8V6yDAksdabCUoVVM3BlpH-NDebxWiJ6mwKw&oe=6408F9BB",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
